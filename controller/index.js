a3a83d6be765f9cb5b5010961700dc213ae7b39a
edsonaraujoneto <edsonaraujodesouzaneto@gmail.com>
2024-01-27 13:19:01 -0300

    atualizando

--- controller/index.js
+++ controller/index.js
@@ -1,6 +1,23 @@
-let buttonComeçar = window.document.querySelector('.efeitosonoro');
-let audioButtonInicial = document.getElementsByTagName('audio');
+let buttonComeçar = document.querySelector('#efeitosonoro');
+let buttonComoJogar = document.querySelector('#efeitosonoro2');
+let buttonCreditos = document.querySelector('#efeitosonoro3');
+let audioArray = document.getElementsByTagName('audio');
 
-buttonInicial.addEventListener('mouseenter', () => {
-    audioButtonInicial[0].play();
+// Esse audio vão tocar quando o usuario passar o mause nos buttons
+buttonComeçar.addEventListener('mouseenter', () => {
+    audioArray[1].play();
 });
+
+buttonComoJogar.addEventListener('mouseenter', () => {
+    audioArray[1].play();
+});
+
+buttonCreditos.addEventListener('mouseenter', () => {
+    audioArray[1].play();
+});
+
+function clicouEmComecar() {
+    audioArray[2].play(); // Quando o usuario clicar em comecar tocara esse audio
+}
+
+
