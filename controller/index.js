849fb6cfc53e4d0cad84dd2dab0db7cd549cdfd9
Alex <Alexsandrosantanafilho@gmail.com>
2024-01-29 19:46:03 -0300

    Adicionando transição quando o jogador clica em começar o jogo

--- controller/index.js
+++ controller/index.js
@@ -2,6 +2,7 @@ let buttonComeçar = document.querySelector('#efeitosonoro');
 let buttonComoJogar = document.querySelector('#efeitosonoro2');
 let buttonCreditos = document.querySelector('#efeitosonoro3');
 let audioArray = document.getElementsByTagName('audio');
+let divPergunta = document.querySelector('.pergunta');
 
 // Esse audio vão tocar quando o usuario passar o mause nos buttons
 buttonComeçar.addEventListener('mouseenter', () => {
@@ -17,7 +18,10 @@ buttonCreditos.addEventListener('mouseenter', () => {
 });
 
 function clicouEmComecar() {
-    audioArray[2].play(); // Quando o usuario clicar em comecar tocara esse audio
+    audioArray[2].play(); // Quando o usuario clicar em comecar tocará esse audio >> E ele será direionado para o painel da primeira pergunta
+    const telaInicial = document.getElementById('telaInicial');
+    telaInicial.style.display = 'none';
+    divPergunta.style.display = 'block';
 }
 
 
