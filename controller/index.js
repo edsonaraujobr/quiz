12d0b1756252dddb7fd4b4c78defa2c8a8db8669
edsonaraujoneto <edsonaraujodesouzaneto@gmail.com>
2024-02-10 01:06:05 -0300

    Resolvi bugs, e deixei responsivo

--- controller/index.js
+++ controller/index.js
@@ -1,14 +1,14 @@
 let buttonComeçar = document.querySelector('#efeitosonoro');
 let buttonComoJogar = document.querySelector('#efeitosonoro2');
 let buttonCreditos = document.querySelector('#efeitosonoro3');
-let audioArray = document.getElementsByTagName('audio');
+let audioArray = document.querySelectorAll('audio');
 let divPergunta = document.querySelector('.pergunta');
-let credits = document.querySelector('#credits');
-let instructions = document.querySelector('#instructions');
+let credits = document.querySelector('.credits');
+let instructions = document.querySelector('.instructions');
 let pergunta1 = document.querySelector('#pergunta1')
 let pergunta2 = document.querySelector('#pergunta2')
 let pergunta3 = document.querySelector('#pergunta3')
-const nome = prompt("Digite seu nome");
+const nome = prompt("Bem vindo ao QuizScript, digite seu nome");
 
 // Esse audio vão tocar quando o usuario passar o mause nos buttons
 buttonComeçar.addEventListener('mouseenter', () => {
@@ -30,44 +30,52 @@ function clicouEmComecar() {
     divPergunta.style.display = 'block';
 }
 
-function clicouEmCreditos() {
-
-    const telaInicial = document.getElementById('telaInicial');
-    telaInicial.style.display = 'none';
-    credits.style.display = 'flex';
-
-}
-
 function clicouEmComoJogar() {
+    audioArray[2].play();
     const telaInicial = document.getElementById('telaInicial');
     telaInicial.style.display = 'none';
     instructions.style.display = 'flex';
 }
 
+function clicouEmCreditos() {
+    audioArray[2].play();
+    const telaInicial = document.getElementById('telaInicial');
+    telaInicial.style.display = 'none';
+    credits.style.display = 'flex';
+}
+
 function acertouAPergunta1() {
+    audioArray[2].play();
     alert(`${nome} parabéns,voce acertou! vamos para a proxima questão?`)
     pergunta1.style.display ='none';
     pergunta2.style.display ='flex';
 }
 
 function errouAPergunta() {
-    alert(`${nome},você errou, tente novamente`)
+    audioArray[4].play();
+    alert(`${nome}, você errou! Tente novamente`)
     pergunta1.style.display ='none';
     pergunta2.style.display ='none';
     pergunta3.style.display ='none';
     telaInicial.style.display = 'flex';
-
-
 }
+
 function acertouAPergunta2() {
-    alert(`${nome} parabéns,voce acertou! vamos para a proxima questão?`)
+    audioArray[2].play();
+    alert(`${nome} parabéns, você acertou! vamos para a proxima questão?`)
     pergunta2.style.display ='none';
     pergunta3.style.display ='flex';
-  }
+}
 
-  function acertouAPergunta3() {
-    alert(`${nome} parabéns por ter chegado até aqui!`)
+function acertouAPergunta3() {
+    audioArray[3].play();
+    alert(`${nome} parabéns por ter chegado até aqui! Você venceu! `)
     pergunta3.style.display ='none'
     telaInicial.style.display = 'flex';
-    console.log("Acertou")
-  }
+}
+
+function clicouEmVoltarTelaIniciaL() {
+    credits.style.display = 'none';
+    instructions.style.display = 'none';
+    telaInicial.style.display = 'flex';
+}
